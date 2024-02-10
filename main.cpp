6125d0862e4bc7a44ed9a1e6182991dd5fe8636a
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-02-10 18:32:52 +0900

    クリア

--- main.cpp
+++ main.cpp
@@ -5,6 +5,37 @@ using std::cin;
 
 
 int main() {
+	int n, x;
+
+	while (true) {
+		cin >> n >> x;
+		if ((n == 0) && (x == 0)) {
+			break;
+		}
+
+		int count = 0;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i == j) {
+					break;
+				}
+				for (int k = 1; k <= n; k++) {
+					if ((i == k) || (j == k)) {
+						break;
+					}
+
+					if ((i + j + k) == x) {
+						count++;
+					}
+
+				}
+			}
+		}
+
+		cout << count << endl;
+
+	};
 
 	return 0;
 }
