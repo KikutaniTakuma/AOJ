761c0d7c2590e2222a7a5417b74942b23cb901c6
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-02-21 20:11:14 +0900

    totiu

--- main.cpp
+++ main.cpp
@@ -2,9 +2,15 @@
 using std::cout;
 using std::endl;
 using std::cin;
-
+#include <string>
 
 int main() {
+	std::string s;
+	std::string p;
+
+	cin >> s >> p;
+
+	cout << (p.find(p) == std::string::npos) ? "Yes" : "No";
 
 	return 0;
 }
