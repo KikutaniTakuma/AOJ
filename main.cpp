51e35185d9c037215cfd577f22ac802c29d20ecb
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-02-21 16:12:08 +0900

    abababa

--- main.cpp
+++ main.cpp
@@ -2,9 +2,42 @@
 using std::cout;
 using std::endl;
 using std::cin;
+#include <string>
+#include <vector>
+
+char ChangeCharScale(char c) {
+	if (not ('a' <= c && c <= 'z') && 
+		not ('A' <= c && c <= 'Z')
+		) {
+		return c;
+	}
+
+	const int toSmallChar = static_cast<int>('a') - static_cast<int>('A');
+	const int bigCharMax = static_cast<int>('Z');
+
+	const int cInt = static_cast<int>(c);
+
+	if (bigCharMax < cInt) {
+		return static_cast<char>(cInt - toSmallChar);
+	}
+	else {
+		return static_cast<char>(cInt + toSmallChar);
+	}
+}
 
 
 int main() {
+	std::string input;
+
+	std::getline(cin, input);
+
+	std::string result;
+
+	for (char& i : input) {
+		result += ChangeCharScale(i);
+	}
+
+	cout << result << endl;
 
 	return 0;
 }
