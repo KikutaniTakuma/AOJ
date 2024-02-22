c9cc6a6b09047c3531a2e17728c8b031ae26a025
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-02-22 16:54:05 +0900

    dekita

--- main.cpp
+++ main.cpp
@@ -2,9 +2,37 @@
 using std::cout;
 using std::endl;
 using std::cin;
-
+#include <string>
 
 int main() {
+	std::string w;
+	std::string T = "BEGIN_OF_TEXT";
+	int count = 0;
+
+	cin >> w;
+
+	std::string tmpW;
+
+	for (auto& i : w) {
+		tmpW += std::tolower(i);
+	}
+
+	w = std::move(tmpW);
+
+	while (T != "END_OF_TEXT") {
+		cin >> T;
+
+		std::string tmpT;
+		for (auto& i : T) {
+			tmpT += std::tolower(i);
+		}
+
+		if (tmpT == w) {
+			count++;
+		}
+	}
+
+	cout << count << endl;
 
 	return 0;
 }
