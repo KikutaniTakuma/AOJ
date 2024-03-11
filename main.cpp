400a99350a33be684c10d3c217be7e6175c20203
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-03-11 13:50:46 +0900

    unchi

--- main.cpp
+++ main.cpp
@@ -3,59 +3,30 @@ using std::cout;
 using std::endl;
 using std::cin;
 #include <string>
-#include <vector>
-
-class Data {
-public:
-	void SetShuffleNumber(const std::string& str) {
-		shuffle_.push_back(std::atoi(str.c_str()));
-	}
-
-	void SetStr(const std::string& str) {
-		str_ = str;
-	}
-
-	void Shuffle() {
-		for (auto& i : shuffle_) {
-			for (size_t j = 0; j < i; j++) {
-				char tmp = str_.front();
-				str_.erase(str_.begin());
-				str_ += tmp;
-			}
-		}
-	}
-
-	void Draw() {
-		cout << str_ << endl;
-	}
-
-	void Reset() {
-		str_.clear();
-		shuffle_.clear();
-	}
-
-private:
-	std::string str_;
-
-	std::vector<int32_t> shuffle_;
-};
 
 int main() {
 	std::string input;
-	std::vector<Data> data;
-	while (std::getline(cin, input)) {
+	size_t count;
+	while (true) {
+		std::cin >> input >> count;
+
 		if (input == "-") {
 			break;
 		}
 
-		for (const char& i : input) {
-			if (not std::isalpha(i)) {
+		for (size_t i = 0; i < count; i++) {
+			int shuffle = 0;
+			std::cin >> shuffle;
 
-				break;
+			for (int j = 0; j < shuffle;j++) {
+				char tmp = input.front();
+				input.erase(input.begin());
+				input += tmp;
 			}
 		}
-	}
 
+		cout << input << endl;
+	}
 
 	return 0;
 }
