d6739812b0e0a0d6e9a9c1e9eb596db263d2a4c3
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-03-11 18:18:18 +0900

    unchi

--- main.cpp
+++ main.cpp
@@ -2,9 +2,26 @@
 using std::cout;
 using std::endl;
 using std::cin;
-
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <cmath>
 
 int main() {
+	double a, b, C;
+
+	cin >> a >> b >> C;
+	C *= M_PI / 180.0;
+
+	double S, L, H;
+
+	S = 0.5 * a * b * std::sin(C);
+
+	L = std::sqrt(a * a + b * b - 2.0 * a * b * std::cos(C)) + a + b;
+
+	H = b * std::sin(C);
+
+	cout << std::fixed << S << endl << L << endl << H;
+
 
 	return 0;
 }
