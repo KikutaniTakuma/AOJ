f7ba6295a1ca30459a6af596cb2a1ea1e559c5a2
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-03-11 16:54:19 +0900

    unchi

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 using std::cout;
 using std::endl;
 using std::cin;
+#include <cmath>
 
 
 int main() {
+	double x, y;
+	double x1, y1;
+
+	cin >> x >> y >> x1 >> y1;
+
+	cout << std::fixed << std::sqrt(pow(y - y1, 2.0) + pow(x - x1, 2.0)) << endl;
+
 
 	return 0;
 }
